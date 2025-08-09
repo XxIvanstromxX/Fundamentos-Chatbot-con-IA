b4c2f7355418143d411f29e161a0ff2ee5a9344a
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-08-08 22:39:18 -0600

    Agregar controlador de productos con métodos para obtener y crear productos

---
+++ class_3/controllers/productController.js
@@ -0,0 +1,52 @@
+const Product = require("../models/productModel");
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ message: "Server ERROR" });
+  }
+};
+
+const createProduct = async (req, res) => {
+  const { name, description, size, stock, price, brand, category, color } =
+    req.body;
+
+  if (
+    !name ||
+    !description ||
+    !size ||
+    !stock ||
+    !price ||
+    !brand ||
+    !category ||
+    !color
+  ) {
+    res.status(400).json({ message: "todos los campos son requeridos" });
+  }
+
+  try {
+    const newProduct = new Product({
+      name,
+      description,
+      size,
+      stock,
+      price,
+      brand,
+      category,
+      color,
+    });
+
+    await newProduct.save();
+    return res
+      .status(201)
+      .json({ message: "Se creo con exito", product: newProduct });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "server error", error: err.message });
+  }
+};
+
+module.exports = { getAllProducts, createProduct };

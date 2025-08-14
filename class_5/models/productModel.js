d8f6822a3373d230c5acf5fd56e708fbc3c660cc
XxIvanstromxX <amembrillom1800@alumno.ipn.mx>
2025-08-13 23:02:43 -0600

    Agregar modelo de producto con validaciones y campos necesarios para la base de datos.

---
+++ class_5/models/productModel.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  // Validaciones básicas de campos
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  size: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  brand: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  color: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
